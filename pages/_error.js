ec3d1cc5961343fc8448ee853dae350778b44227
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-02-19 12:36:49 +0530

    add error page

---
+++ pages/_error.js
@@ -0,0 +1,12 @@
+
+import Layout from '../components/Layout';
+
+export default () => {
+  return (
+    <Layout title="Not found!">
+      <div className="w-1/3 mt-20">
+        Error
+      </div>
+    </Layout>
+  );
+};
