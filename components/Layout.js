1ade69a8e9a22725b53a8bd5caa06304b0962b67
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2019-02-19 12:06:56 +0530

    Add nprogress
    Indicate progress on page transitions

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,16 @@
 import Head from 'next/head';
+import Router from 'next/router';
 import { useContext } from 'react';
+import Nprogress from 'nprogress';
 
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
-
 import { ThemeContext } from '../contexts/ThemeContext';
+
+Router.onRouteChangeStart = url => Nprogress.start();
+Router.onRouteChangeComplete = () => Nprogress.done();
+Router.onRouteChangeError = () => Nprogress.done();
 
 const Layout = ({ title, children }) => {
   const { theme } = useContext(ThemeContext);
